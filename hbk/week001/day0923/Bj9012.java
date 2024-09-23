b072f693e19f839d4dbd629a1a4a6eff3c3cb946
TalkingPotato90 <star901210@hanmail.net>
2024-09-23 18:53:12 +0900

    Feat: Week001 0923 String

---
+++ hbk/week001/day0923/Bj9012.java
@@ -0,0 +1,46 @@
+package week001.day0923;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Bj9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int number = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < number; i++) {
+            sb.append(judge(br.readLine()));
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static String judge(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            }else if(c == ')'){
+                if(stack.isEmpty()){
+                    return "NO";
+                }else if(stack.peek() == '('){
+                    stack.pop();
+                }
+            }
+        }
+
+        if(stack.isEmpty()){
+            return "YES";
+        }else {
+            return "NO";
+        }
+    }
+}
