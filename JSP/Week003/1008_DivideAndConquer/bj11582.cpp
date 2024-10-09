88bbe62ec641dda2de0b1afb03e29cc8f2eff9af
ChrisP <jspark4151@gmail.com>
2024-10-09 17:07:21 +0900

    Refactor: Week003 1008 Divide And Conquer bj11582

--- JSP/Week003/1008_DivideAndConquer/bj11582.cpp
+++ JSP/Week003/1008_DivideAndConquer/bj11582.cpp
@@ -1,85 +1,84 @@
 #include <iostream>
-#include <vector>
 #define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 
 using namespace std; 
 
-vector<int> arr;
-vector<int> tempArr;
 int n, k;
 
-void merge(int left, int mid, int right)
+void merge(int left, int mid, int right, int* arrPtr)
 {
     if((right - left) > (n / k))
     {
         return;
     }
 
+    int* tempArrPtr = new int[right - left + 1];
+
     int leftIdx = left; 
     int midIdx = mid + 1;
-    int tempIdx = left;
+    int tempIdx = 0;
 
     while(leftIdx <= mid && midIdx <= right)
     {
-        arr[leftIdx] <= arr[midIdx] ? 
-        tempArr[tempIdx++] = arr[leftIdx++] : 
-        tempArr[tempIdx++] = arr[midIdx++];
+        arrPtr[leftIdx] <= arrPtr[midIdx] ? 
+        tempArrPtr[tempIdx++] = arrPtr[leftIdx++] : 
+        tempArrPtr[tempIdx++] = arrPtr[midIdx++];
     }
 
     // 왼쪽 서브어레이가 남았을 경우 
     while(leftIdx <= mid)
     {
-        tempArr[tempIdx++] = arr[leftIdx++];
+        tempArrPtr[tempIdx++] = arrPtr[leftIdx++];
     }
 
     // 오른쪽 서브어레이가 남았을 경우
     while(midIdx <= right)
     {
-        tempArr[tempIdx++] = arr[midIdx++];
+        tempArrPtr[tempIdx++] = arrPtr[midIdx++];
     }
 
     // 임시 배열의 값을 정렬 배열에 저장
     for(int i = left; i <= right; ++i)
     {
-        arr[i] = tempArr[i]; 
+        arrPtr[i] = tempArrPtr[i - left]; 
     }
 
+    delete(tempArrPtr);
 }
 
-void mergeSort(int left, int right)
+void mergeSort(int left, int right, int* arrPtr)
 {
     if(left < right)
     {
         int mid = (left + right) / 2;
-        mergeSort(left, mid);
-        mergeSort(mid + 1, right);
-        merge(left, mid, right);
+        mergeSort(left, mid, arrPtr);
+        mergeSort(mid + 1, right, arrPtr);
+        merge(left, mid, right, arrPtr);
     }
 }
-
 
 int main()
 {
     fast;
 
     cin >> n; 
-
-    arr.resize(n);
-    tempArr.resize(n);
+    int* arrPtr = new int[n];
 
     for (int i = 0; i < n; ++i) 
     {
-        cin >> arr[i];
+        int n;
+        cin >> arrPtr[i];
     }
 
     cin >> k;
 
-    mergeSort(0, n - 1);
+    mergeSort(0, n - 1, arrPtr);
 
     for(int i = 0; i < n; ++i)
     {
-        cout << arr[i] << ' ';
+        cout << arrPtr[i] << ' ';
     }
 
+    delete(arrPtr);
     return 0;
 }
