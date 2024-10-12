f4780e13436f35c45e70f00e225b0b6f30838165
ChrisP <jspark4151@gmail.com>
2024-10-12 22:29:04 +0900

    Fix: Week003 1011 Implementation

--- JisooPark/Week003/1011_Implementation/bj21608.cpp
+++ JisooPark/Week003/1011_Implementation/bj21608.cpp
@@ -4,12 +4,13 @@
 
 using namespace std;
 
-vector<vector<int>> dir = {{-1, 0},  {0,-1}, {1, 0}, {0, 1}};
-vector<vector<int>> classroom; 
+// 왼 -> 위 -> 우 -> 아래
+vector<vector<int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+vector<vector<pair<int, int>>> classroom; 
 vector<vector<int>> students; 
 int n;
 
-pair<int, int> checkSeat(int y, int x, int student)
+pair<int, int> checkSeat(int y, int x, int idx)
 {
     int favorite = 0; 
     int emptySeat = 0;
@@ -20,12 +21,12 @@ pair<int, int> checkSeat(int y, int x, int student)
         int nx = x + dir[d][1];
 
         // 범위를 벗어나나? 
-        if(ny < 0 || ny > n || nx < 0 || nx > n)
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n)
         {
             continue;
         }
 
-        if(classroom[ny][nx] == 0)
+        if(classroom[ny][nx].first == 0)
         {
             emptySeat++;
         }
@@ -33,7 +34,7 @@ pair<int, int> checkSeat(int y, int x, int student)
         {
             for(int f = 1; f < 5; ++f)
             {
-                if(classroom[ny][nx] == students[student][f])
+                 if(classroom[ny][nx].first == students[idx][f])
                 {
                     favorite++;
                     break;
@@ -48,37 +49,18 @@ pair<int, int> checkSeat(int y, int x, int student)
 int checkValue()
 {
     int ans = 0;
-
+    int idx = 0;
     // 모든 자리를 확인해 감
     for(int y = 0; y < n; ++y)
     {
         for(int x = 0; x < n; ++x)
         {
-            int s = classroom[y][x];
+            int idx = classroom[y][x].second;
             int favorite = 0;
 
-            for(int d = 0; d < 4; ++d)
-            {
-                int ny = y + dir[d][0];
-                int nx = x + dir[d][1];
+            pair<int, int> temp = checkSeat(y, x, idx);
 
-                // 범위를 벗어나나? 
-                if(ny < 0 || ny > n || nx < 0 || nx > n)
-                {
-                    continue;
-                }
-
-                for(int f = 1; f < 5; ++f)
-                {
-                    if(classroom[ny][nx] == students[s][f])
-                    {
-                        favorite++;
-                        break;
-                    }
-                }
-            }
-
-            ans += pow(10, favorite - 1);
+            ans += pow(10, temp.first - 1);
         }
     }
 
@@ -89,13 +71,9 @@ int main()
 {
     cin >> n;
 
-    classroom.assign(n, vector<int>(n, 0));
+    classroom.assign(n, vector<pair<int, int>>(n, {0,0}));
     students.assign(n * n + 1, vector<int>(5));
 
-    // student[f][s]
-    //  f == 학생 앉는 순서
-    // s 의 배열 -> 0 학생, 1~4 친구
-    
     int count = n * n;
 
     for(int i = 0; i < count; ++i)
@@ -109,13 +87,11 @@ int main()
         }
     }
 
-    classroom[1][1] = students[0][0];
+    classroom[1][1].first = students[0][0];
+    classroom[1][1].second = 0;
 
     for(int idx = 1; idx < count; ++idx)
     {
-        // 자리에 앉을 학생
-        int s = students[idx][0];
-
         // 가장 좋았던 위치
         int mostFavorite = 0; 
         int emptySeat = -1;
@@ -127,13 +103,12 @@ int main()
         {
             for(int x = 0; x < n; ++x)
             {
-                if(classroom[y][x] != 0)
+                if(classroom[y][x].first != 0)
                 {
                     continue;
                 }
-
                 // 현재 자리의 값어치를 계산
-                pair<int,int> value = checkSeat(y, x, s);
+                pair<int,int> value = checkSeat(y, x, idx);
 
                 // 좋아하는 친구가 제일 많음
                 if(value.first > mostFavorite)
@@ -158,19 +133,10 @@ int main()
             }
         }
 
-        classroom[fy][fx] = s;
+        classroom[fy][fx].first = students[idx][0];
+        classroom[fy][fx].second = idx;
     }
 
-    for(int y = 0; y < n; ++y)
-    {
-        for(int x = 0; x < n; ++x)
-        {
-            cout << classroom[y][x] << ' ';
-        }
-
-        cout << '\n';
-    }
-
-    // int answer = checkValue();
-    // cout << answer;
+    int answer = checkValue();
+    cout << answer;
 }
