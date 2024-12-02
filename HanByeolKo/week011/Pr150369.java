98fc7c00b68dab572f6764e778399a5498007aa0
TalkingPotato90 <star901210@hanmail.net>
2024-12-02 21:22:01 +0900

    Feat: Week011 1202 TestBank

---
+++ HanByeolKo/week011/Pr150369.java
@@ -0,0 +1,27 @@
+package week011;
+
+class Pr150369 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+
+        int d = 0;
+        int p = 0;
+
+        for(int i = n-1; i >= 0; i--){
+            int count = 0;
+
+            d -= deliveries[i];
+            p -= pickups[i];
+
+            while (d < 0 || p < 0){
+                d += cap;
+                p += cap;
+                count++;
+            }
+
+            answer += (i + 1) * 2 * count;
+        }
+
+        return answer;
+    }
+}
