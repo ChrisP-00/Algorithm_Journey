5a2de06c9f8cb6ffb1a432a81d83db13f0269f02
TalkingPotato90 <star901210@hanmail.net>
2024-12-04 20:45:49 +0900

    Feat: Week011 1204 Graph

---
+++ HanByeolKo/week011/day1204_Graph/Bj2178.java
@@ -0,0 +1,63 @@
+package week011.day1204_Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Bj2178 {
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    private static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int result = bfs(n, m);
+        System.out.println(result);
+
+    }
+
+    private static int bfs(int n, int m) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0});
+        graph[0][0] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            if(x == n-1 && y == m-1) {
+                return graph[x][y];
+            }
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
