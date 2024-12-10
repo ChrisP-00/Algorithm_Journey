a6b9f7533d44bfe067ecb708e504ae9a02e7b400
ChrisP-00 <jspark4151@gmail.com>
2024-12-10 12:50:48 +0900

    Refactor: Week012 1209 bj21758

--- JisooPark/Week012/1209_PrefixSum/bj21758.cpp
+++ JisooPark/Week012/1209_PrefixSum/bj21758.cpp
@@ -9,8 +9,8 @@ int main() {
     int n;
     cin >> n;
 
-    int honey[100002] = {0,};
-    int prefixSum[100002] = {0, };
+    int honey[100002] = {0};
+    int prefixSum[100002] = {0};
 
     // 입력 및 누적 합 계산
     for (int i = 1; i <= n; ++i) {
@@ -21,13 +21,13 @@ int main() {
     int answer = 0;
 
     for (int idx = 2; idx < n; ++idx) {
-        // 벌[1] - 벌통[idx] - 벌[n]
+        // 벌[1] - 벌2[idx] - 벌통[n]
         answer = max(answer, 2 * prefixSum[n] - honey[1] - honey[idx] - prefixSum[idx]);
 
         // 벌통[1] - 벌1[idx] - 벌2[n]
         answer = max(answer, prefixSum[n - 1] - honey[idx] + prefixSum[idx - 1]);
 
-        // 벌[1] - 벌2[idx] - 벌통[n]
+        // 벌[1] - 벌통[idx] - 벌[n]
         answer = max(answer, prefixSum[idx] - honey[1] + prefixSum[n - 1] - prefixSum[idx - 1]);
     }
 
