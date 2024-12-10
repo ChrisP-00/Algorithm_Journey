e84556bf718e7b9584c074ffd310d92db7085e84
TalkingPotato90 <star901210@hanmail.net>
2024-12-10 16:46:34 +0900

    Feat: Week012 1210 PrefixSum

---
+++ HanByeolKo/week012/day1210_PrefixSum/Bj2167.java
@@ -0,0 +1,52 @@
+package week012.day1210_PrefixSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj2167 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int startX, startY, endX, endY;
+
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            startX = Integer.parseInt(st.nextToken()) - 1;
+            startY = Integer.parseInt(st.nextToken()) - 1;
+            endX = Integer.parseInt(st.nextToken()) - 1;
+            endY = Integer.parseInt(st.nextToken()) - 1;
+
+            int sum = calculateSum(arr, startX, startY, endX, endY);
+
+            System.out.println(sum);
+        }
+    }
+
+    public static int calculateSum(int[][] arr, int sX, int sY, int eX, int eY) {
+        int sum = 0;
+
+        for (int i = sX; i <= eX; i++) {
+            for (int j = sY; j <= eY; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ HanByeolKo/week012/day1210_PrefixSum/Bj2851.java
@@ -0,0 +1,39 @@
+package week012.day1210_PrefixSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2851 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] mushroom = new int[10];
+        int sum = 0;
+        int[] total = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            mushroom[i] = Integer.parseInt(br.readLine());
+            sum += mushroom[i];
+            total[i] = sum;
+        }
+
+        int small = 0;
+        int large = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (total[i] > 100) {
+                large = total[i];
+                small = total[i - 1];
+                break;
+            }
+            large = total[i];
+        }
+
+        int smallDif = Math.abs(100 - small);
+        int largeDif = Math.abs(100 - large);
+
+        int result = smallDif < largeDif ? small : large;
+
+        System.out.println(result);
+    }
+}
