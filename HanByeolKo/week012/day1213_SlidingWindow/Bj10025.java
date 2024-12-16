dda525a718d2af8c7aa8d39ebb788602117d8f36
TalkingPotato90 <star901210@hanmail.net>
2024-12-16 19:23:42 +0900

    Feat: Week012 1213 SlidingWindow

---
+++ HanByeolKo/week012/day1213_SlidingWindow/Bj10025.java
@@ -0,0 +1,42 @@
+package week012.day1213_SlidingWindow;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj10025 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int range = 2 * k + 1;
+
+        int[] ice = new int[1000000];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int g = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            ice[x] += g;
+        }
+
+        int sum = 0;
+        for (int i = 0; i <= range && i < 1000000; i++) {
+            sum += ice[i];
+        }
+
+        int max = sum;
+
+        for (int i = range + 1; i < 1000000; i++) {
+            sum += ice[i];
+            sum -= ice[i - range - 1];
+            max = Math.max(sum, max);
+        }
+
+        System.out.println(max);
+    }
+}
