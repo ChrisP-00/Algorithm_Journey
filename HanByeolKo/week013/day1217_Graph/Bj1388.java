fd19ef10afec420dbde209bcd01a0ee0fd24e957
TalkingPotato90 <star901210@hanmail.net>
2024-12-17 13:39:11 +0900

    Feat: Week013 1217 Graph

--- HanByeolKo/week013/day1217_Graph/Bj1388.java
+++ HanByeolKo/week013/day1217_Graph/Bj1388.java
@@ -13,16 +13,16 @@ public class Bj1388 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] wood = new int[n + 1][m + 1];
+        char[][] wood = new char[n + 1][m + 1];
 
         int count = 0;
 
         for (int i = 1; i <= n; i++) {
             String input = br.readLine();
             for (int j = 1; j <= m; j++) {
-                wood[i][j] = input.charAt(j - 1) == '-' ? 1 : 2;
+                wood[i][j] = input.charAt(j - 1);
 
-                if ((wood[i][j] == 1 && wood[i][j - 1] == 1) || (wood[i][j] == 2 && wood[i - 1][j] == 2)) {
+                if ((wood[i][j] == '-' && wood[i][j - 1] == '-') || (wood[i][j] == '|' && wood[i - 1][j] == '|')) {
                     continue;
                 }
 
