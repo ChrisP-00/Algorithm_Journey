e65dc0a49c0022dc101324933773c2045bcb5130
TalkingPotato90 <star901210@hanmail.net>
2025-02-08 00:04:00 +0900

    Feat: Week018 0124 Implementation

--- HanByeolKo/week018/day0124_Implementation/Bj1138.java
+++ HanByeolKo/week018/day0124_Implementation/Bj1138.java
@@ -1,4 +1,35 @@
 package week018.day0124_Implementation;
 
+import java.util.Scanner;
+
 public class Bj1138 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] result = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int pos = arr[i];
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (result[j] == 0) {
+                    if (count == pos) {
+                        result[j] = i + 1;
+                        break;
+                    }
+                    count++;
+                }
+            }
+        }
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
